605233e53b71e774f45ab38dc49bfc696e43d141
Amir Muminov <amir_9999@mail.ru>
2020-11-13 23:53:26 +0600

    zuul with security

---
+++ src/main/java/kz/muminov/zuul/security/SecurityTokenConfig.java
@@ -0,0 +1,25 @@
+package kz.muminov.zuul.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .addFilterAfter(new JwtTokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                    .antMatchers( HttpMethod.POST,"/auth/**").permitAll()
+                    .anyRequest().authenticated();
+    }
+
+}
